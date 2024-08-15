f9eaec03d1715712369625fd3f173f622efa3e24
SiFrancis <franzo08112007@gmail.com>
2024-08-15 08:52:37 +0800

    updated FA7

--- 1stQ/Sitchon_FA7.py
+++ 1stQ/Sitchon_FA7.py
@@ -8,13 +8,7 @@ def main():
     day = int(input("Key in the day of the month: "))
     month = int(input("Key in the month number (1 = Jan, 2 = Feb, ...): "))
     year = int(input("Key in the year: "))
-
-    y_prime = year - (14 - month) // 12
-    leap = y_prime + y_prime // 4 - y_prime // 100 + y_prime // 400
-    m_prime = month + 12 * ((14 - month) // 12) - 2
-    d_prime = (day + leap + (31 * m_prime) // 12) % 7
     
-    # My code here
     d = dt.datetime(year, month, day)
     print(f"{d:%d} {d:%B} {year} is a {d:%A}")
 
